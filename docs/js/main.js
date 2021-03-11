da5eb4fb6c7ea0490f8027d2bdb0918c75dc0252
Nastya <the_sun.89@mail.ru>
2021-03-11 21:20:46 +0300

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,31 @@
+let fullSurname = prompt("Напишите фамилию",'');
+	
+	while (fullSurname === null || fullSurname === '' ) {
+		fullSurname = prompt("Введите данные корректно.Напишите фамилию", '');
+ 	}
+
+	 let fullName = prompt("Напишите имя",'');
+	
+	 while ( fullName === null || fullName === ''  ){
+		 fullName = prompt("Введите данные корректно.Напишите имя", '');
+	  }
+ 
+	  let patronymic = prompt("Напишите отчество",'');
+	
+	  while (patronymic === null || patronymic === '' ) {
+		patronymic = prompt("Введите данные корректно.Напишите отчество", '');
+	   }
+
+let age = parseInt(prompt('Введите Ваш возраст:', ''));
+
+    
+	while ((Number.isNaN(age)) === true || age >= 150 || age <= 0) {
+		age = parseInt(prompt("Пишите правильный возраст в цифрах", ));
+
+	}
+let gender = confirm('Ваш пол - мужской?') ? 'мужской':'женский';
+
+if (((gender ==='мужской')&&(age >= 62))||((gender ==='женский')&&(age >= 58))) {
+    you_retired = "да";
+} else { you_retired = "нет";}
+alert("Ваше ФИО:"+"  " + fullSurname + " " + fullName + " "+patronymic + "\n" + "Ваш возраст в годах :" +" "+ age  + "\n" + "Ваш возраст в днях:" +" " +(age*365)  + "\n" + "Через 5 лет Вам будет:" + " " +(age+5)  + "\n" + "Ваш пол:" + " " + gender + "\n" + "Вы на пенсии:" +" " + you_retired);
